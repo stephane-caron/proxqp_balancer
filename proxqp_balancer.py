8fe31d7048014c738d2a86fbb9a8373d3a84811b
Stéphane Caron <stephane.caron@inria.fr>
2024-10-11 16:46:55 +0200

    Fix wheel odometry

--- proxqp_balancer.py
+++ proxqp_balancer.py
@@ -432,8 +432,8 @@ if __name__ == "__main__":
         spine_config={
             "wheel_odometry": {
                 "signed_radius": {
-                    "left_wheel": -WHEEL_RADIUS,
-                    "right_wheel": +WHEEL_RADIUS,
+                    "left_wheel": +WHEEL_RADIUS,
+                    "right_wheel": -WHEEL_RADIUS,
                 }
             }
         },
